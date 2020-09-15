35f0dec6eae55b1a29305897f7dc608db09cfa80
jahs <jahs.es@gmail.com>
2020-09-15 16:53:37 +0200

    Adds migration

---
+++ migrations/02_create_patient.up.sql
@@ -0,0 +1,14 @@
+create table patient
+(
+id varchar(36),
+name varchar(150) not null,
+address varchar(200),
+email varchar(50),
+phone varchar(50) not null,
+active bit default 1,
+created_at datetime not null,
+created_by varchar(50) not null,
+updated_at datetime,
+updated_by varchar(50),
+PRIMARY KEY (`id`))
+ENGINE=InnoDB DEFAULT CHARSET=latin1;
---
+++ migrations/03_create_admin.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO clinic.user (id, email, password, name, rol, created_at) VALUES('b5d134cc-12cc-46a8-bc48-64e8f2520026','jahs.es@gmail.com', '$2y$12$wLH6EMA8okangy4L9FlOO.oMrBhYEcZpEjljSSNGE8Zar2nJx3KNm', 'admin', 'admin', current_date());
+
+
+
