917706d93b235672ec3b51be09631b169249395f
jahs <jahs.es@gmail.com>
2020-09-15 16:58:06 +0200

    Adds middleware

---
+++ adapter/middleware/authentication.go
@@ -0,0 +1,16 @@
+package middleware
+
+import (
+	"github.com/jahs/clinic-backend/adapter/auth"
+	"net/http"
+)
+
+func Authentication(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	err := auth.TokenValid(r)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Unauthorized"))
+		return
+	}
+		next(w, r)
+}
