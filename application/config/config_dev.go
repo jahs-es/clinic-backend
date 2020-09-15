8a55d1beda177e22c10506fb08b92747bd45fd47
jahs <jahs.es@gmail.com>
2020-09-15 16:58:16 +0200

    Adds config

---
+++ application/config/config_dev.go
@@ -0,0 +1,13 @@
+// +build dev
+
+package config
+
+const (
+	DB_USER                = "root"
+	DB_PASSWORD            = "root001"
+	DB_DATABASE            = "clinic"
+	DB_HOST                = "localhost"
+	//DB_HOST                = "mysql"
+	API_PORT               = 8080
+	PROMETHEUS_PUSHGATEWAY = "http://localhost:9091/"
+)
---
+++ application/config/config_testing.go
@@ -0,0 +1,12 @@
+// +build testing
+
+package config
+
+const (
+	DB_USER                = "root"
+	DB_PASSWORD            = "root001"
+	DB_DATABASE            = "clinic"
+	DB_HOST                = "127.0.0.1"
+	API_PORT               = 8080
+	PROMETHEUS_PUSHGATEWAY = "http://localhost:9091/"
+)
