fa367f34a57c7139f757f54152ee580567846bc3
jahs <jahs.es@gmail.com>
2020-09-23 14:49:04 +0200

    Admin with pass admin

--- migrations/03_create_admin.up.sql
+++ migrations/03_create_admin.up.sql
@@ -1,4 +1,4 @@
-INSERT INTO clinic.user (id, email, password, name, rol, created_at) VALUES('b5d134cc-12cc-46a8-bc48-64e8f2520026','jahs.es@gmail.com', '$2y$12$wLH6EMA8okangy4L9FlOO.oMrBhYEcZpEjljSSNGE8Zar2nJx3KNm', 'admin', 'admin', current_date());
+INSERT INTO clinic.user (id, email, password, name, rol, created_at) VALUES('b5d134cc-12cc-46a8-bc48-64e8f2520026','jahs.es@gmail.com', '$2y$12$y0nkHderAiMaahtS9R825.JPFPj555QsVOipR0L9xsluz8/MxnRQO', 'admin', 'admin', current_date());
 
 
 
